721e48395b53f2f9beb4a7d99d52f4ff1bbac9f0
Robert Vo <>
2020-08-17 22:42:22 +0700

    Updates

---
+++ precache-manifest.8731d9faeece29f01f3df962e24e6c31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934ee26931d563ec977efcb56d7cb4cf",
+    "url": "/fullerton-booking/index.html"
+  },
+  {
+    "revision": "c34e06100d9a2d261c0a",
+    "url": "/fullerton-booking/static/css/2.b56a0318.chunk.css"
+  },
+  {
+    "revision": "c847ca3968f7ae65cd0a",
+    "url": "/fullerton-booking/static/css/main.6a3f60d4.chunk.css"
+  },
+  {
+    "revision": "c34e06100d9a2d261c0a",
+    "url": "/fullerton-booking/static/js/2.f91c8848.chunk.js"
+  },
+  {
+    "revision": "7b1affef45e2b57a268c1974299a48a9",
+    "url": "/fullerton-booking/static/js/2.f91c8848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c847ca3968f7ae65cd0a",
+    "url": "/fullerton-booking/static/js/main.afccb77b.chunk.js"
+  },
+  {
+    "revision": "1290562ae48a083b7b77",
+    "url": "/fullerton-booking/static/js/runtime-main.0044f1ba.js"
+  }
+]);
